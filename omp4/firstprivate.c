11c0fee12a4f7a8edb5734a3185a195f795c25c9
compparalela <compparalela@gmail.com>
2014-05-10 19:05:05 -0300

    firstprivate

---
+++ omp4/firstprivate.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+  int idx = 100;
+  #pragma omp parallel firstprivate(idx)
+  {
+    printf("In thread %d idx = %d\n", omp_get_thread_num(), idx);
+  }
+}
